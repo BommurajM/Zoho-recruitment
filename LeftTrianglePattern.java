eb56498db95930bcfebe8c0adaa0e0aa9478648c
BommurajM <74760077+BommurajM@users.noreply.github.com>
2021-02-26 09:29:51 +0530

    Add files via upload

---
+++ LeftTrianglePattern.java
@@ -0,0 +1,18 @@
+import java.util.io.*;
+public class LeftTrianglePattern {
+public static void main(String args[]) {
+  int i,j,row=6;
+for(i=0; i<row; i++)
+{
+for(j=2*(row-i); j>=0; j--)
+{
+ System.out.print("");
+}
+for(j=0; j<=i; j++)
+{
+	System.out.print("Water");
+}
+System.out.print();
+}
+}
+}
